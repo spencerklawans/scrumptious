e19f5690a4386dc6cde8c039717643419fc16013
reillynski <43476619+reillynski@users.noreply.github.com>
2020-05-25 10:25:19 -0700

    connecting ui pages

--- src/main/java/com/vaadin/tutorial/crm/ui/ProjectView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/ProjectView.java
@@ -7,6 +7,8 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.tutorial.crm.ui.AddProjectComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 /**
  * A Designer generated component for the main-project-view template.
@@ -21,6 +23,10 @@ public class ProjectView extends PolymerTemplate<ProjectView.ProjectViewModel> {
 
     @Id("header")
 	private HeaderComponent header;
+	@Id("addButton")
+	private AddProjectComponent addButton;
+	@Id("topLayout")
+	private HorizontalLayout topLayout;
 
 	/**
      * Creates a new MainProjectView.
@@ -29,6 +35,7 @@ public class ProjectView extends PolymerTemplate<ProjectView.ProjectViewModel> {
         // You can initialise any data required for the connected UI components here.
     	header.setLogo();
     	header.setUserButton();
+    	addButton.setNavButton();
     }
 
     /**
