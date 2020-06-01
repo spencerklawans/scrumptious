3588415d505d2f448ca2bb7b058b0b60bbeda564
reillynski <43476619+reillynski@users.noreply.github.com>
2020-05-31 22:44:43 -0700

    ui fixes, components scroll and wrap on overflow

--- frontend/src/views/ticket-component.js
+++ frontend/src/views/ticket-component.js
@@ -2,7 +2,6 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
-import '@polymer/iron-icon/iron-icon.js';
 
 class TicketComponent extends PolymerElement {
 
@@ -14,13 +13,11 @@ class TicketComponent extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="border: 2px solid #000000; background-color: #FFFFFF; justify-content: space-between;">
- <vaadin-button id="title" style="width: 100%; background-color: rgba(255, 255, 255); font-size: 14pt; flex-grow: 0;"></vaadin-button>
+<vaadin-vertical-layout style="border: 2px solid #000000; background-color: #FFFFFF; justify-content: space-between; width: 100%; margin: var(--lumo-space-xs);">
+ <vaadin-button id="title" style="width: 100%; background-color: rgba(255, 255, 255); font-size: 14pt; flex-grow: 1; color: #000000; font-weight: bold;" disabled></vaadin-button>
  <vaadin-horizontal-layout style="width: 100%; justify-content: space-between; padding-right: var(--lumo-space-m); padding-left: var(--lumo-space-m);">
-  <vaadin-button id="assignedUser" style="background-color: #FFFFFF; font-size: 8pt;" disabled tabindex="">
-   <iron-icon icon="lumo:user" slot="prefix" style="color: #FED766;"></iron-icon>
-  </vaadin-button>
-  <vaadin-button id="priority" style="font-size: 8pt; color: #000000; background-color: #FFFFFF;" disabled tabindex=""></vaadin-button>
+  <vaadin-button id="assignedUser" style="background-color: #FFFFFF; font-size: 10pt; color: #000000; flex-grow: 1;" disabled tabindex=""></vaadin-button>
+  <vaadin-button id="priority" style="font-size: 10pt; color: #000000; background-color: #FFFFFF; font-weight: bold; flex-grow: 0;" disabled tabindex=""></vaadin-button>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
